096ff2f029369c7f762bc6ed0ea799cad8b46524
Shardul Patil <sjpatil@uci.edu>
2021-02-10 13:31:59 -0800

    Initial Setup

---
+++ NutrioUs/NutrioUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutrioUsApp.swift
+//  NutrioUs
+//
+//  Created by Shardul Patil on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct NutrioUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
